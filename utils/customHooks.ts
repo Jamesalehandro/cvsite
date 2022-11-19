64be7608fe7b6fd2d07e5e35e76da0d92b4b4859
Jamesalehandro <Jamesalehandro>
2022-11-19 19:11:58 +0100

    added test code

---
+++ utils/customHooks.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef, useState } from 'react';
+
+// More practice on intersectionObserver later
+export const useElementOnScreen = (options: IntersectionObserverInit) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const callBackFunction = (entries: IntersectionObserverEntry[]) => {
+    const [entry] = entries;
+    setIsVisible(entry.isIntersecting);
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callBackFunction, options);
+    if (containerRef.current) observer.observe(containerRef.current);
+
+    return () => {
+      if (containerRef.current) observer.unobserve(containerRef.current);
+    };
+  }, [containerRef, callBackFunction]);
+
+  return { containerRef, isVisible };
+};
